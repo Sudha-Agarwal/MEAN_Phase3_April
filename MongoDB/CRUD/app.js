969af963fb9b0359275906db4c9a589deedc41ce
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-05-06 13:05:31 +0530

    06-05-23

--- MongoDB/CRUD/app.js
+++ MongoDB/CRUD/app.js
@@ -10,8 +10,8 @@ mongoose.connect(uri)
 }).catch(err => console.log(err));
 
 
-function searchUser(){
-const User = mongoose.model("User", {
+async function searchUser(){
+const UserSchema = new mongoose.Schema({
     name: {
       type: String,
       required: true
@@ -22,8 +22,8 @@ const User = mongoose.model("User", {
     },
     email: {
       type: String,
-      required: true,
-      unique: true
+      required: true
+     
     },
     active: {
       type: Boolean,
@@ -31,16 +31,28 @@ const User = mongoose.model("User", {
     },
     hobbies:[String]
   });
+  const User = mongoose.model('User', UserSchema);
+  UserSchema.pre('save', function(next){
+    this.name = this.name.toUpperCase();
+    next();//to call next hook
+  })
 
-  const users = [
-    { name: 'Alice', age: 25, email: 'alice@gmail.com', active: true, hobbies: ['reading', 'swimming'] },
-    { name: 'Bob', age: 30, email: 'bob@gmail.com', active: false, hobbies: ['running', 'hiking'] },
+  UserSchema.post('find', function(docs){
+    console.log(`Found ${docs.length} users`);
+  });  
+
+  const users = new User(
+    { name: 'Alice', age: 25, email: 'alice@gmail.com', active: true, hobbies: ['reading', 'swimming'] }
+    /*{ name: 'Bob', age: 30, email: 'bob@gmail.com', active: false, hobbies: ['running', 'hiking'] },
     { name: 'Charlie', age: 20, email: 'charlie@Gmail.com', active: true, hobbies: ['swimming', 'yoga'] },
     { name: 'David', age: 35, email: 'david@yahoo.com', active: true, hobbies: ['reading', 'hiking'] },
-    { name: 'Eve', age: 28, email: 'eve@yahoo.com', active: false, hobbies: ['swimming', 'yoga'] }
-  ];
+    { name: 'Eve', age: 28, email: 'eve@yahoo.com', active: false, hobbies: ['swimming', 'yoga'] }*/
+  );
 
-  User.insertMany(users).then(()=>{
+  await users.save();
+  const result = await User.find();
+
+  /*User.insertMany(users).then(()=>{
     User.find().where('age').gte(18).lte(30)
     .where('email').regex(/@gmail\.com$/i)
     .where('active').equals(true)
@@ -48,7 +60,7 @@ const User = mongoose.model("User", {
     .sort('-age')
     .select('name age email hobbies')
     .limit(1).then(users => console.log(users))
-  });
+  });*/
 }
 
 searchUser();
